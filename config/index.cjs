ec321051787b8aace37d05c0e7ba3130fe740c6e
Dong Yan <jimmy20021113@163.com>
2023-07-26 13:41:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,10 +1,67 @@
 /* eslint-disable */
 
 /**
+
  * 此项目配置为方便新人使用，已缩减至最简配置。
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
- */
-const USER_CONFIG = {"APP_ID":"wx50e84fa34ca6b2c0","APP_SECRET":"c9951636322280eaca625313636cd323","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"g6FJMUy6ypmWtS3xDtpecrVexrqvCIGoFL4kLPTEj3o","CALLBACK_USERS":[{"name":"自己","id":"oib9Z63xYxXx7rIi4mbE1Uw0ubR0"}],"USERS":[{"name":"yd","id":"oib9Z63xYxXx7rIi4mbE1Uw0ubR0","useTemplateId":"pTTJuDziQiJwdJnNtcMJJSHUdDowlibfQsNfghICwXI","province":"上海","city":"上海","horoscopeDate":"11-13","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"儿童节","date":"06-01","year":"2023"}],"customizedDateList":[{"keyword":"恋爱纪念日","date":"2023-04-22"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":false,"oneTalk":false,"earthyLoveWords":true,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
-module.exports = USER_CONFIG
+   */
+   const USER_CONFIG = {
 
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: 'wx50e84fa34ca6b2c0',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: 'c9951636322280eaca625313636cd323',
+
+  PROVINCE: '上海',
+  CITY: '上海',
+
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '宝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oib9Z63xYxXx7rIi4mbE1Uw0ubR0',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'g6FJMUy6ypmWtS3xDtpecrVexrqvCIGoFL4kLPTEj3o',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '11-13',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '1996', date: '09-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2023', date: '04-22',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-04-22' },
+        // 结婚纪念日
+        // { 
+      ],keyword: 'marry_day', date: '2022-09-09' },
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: '',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: '',
+    }
+  ],
+
+}
+
+module.exports = USER_CONFIG
