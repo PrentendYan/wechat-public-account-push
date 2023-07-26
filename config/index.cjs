99103a2435d960f26ba42f054b4726a340f97993
Dong Yan <jimmy20021113@163.com>
2023-07-26 14:17:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,21 +20,21 @@
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝',
+      name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oib9Z63xYxXx7rIi4mbE1Uw0ubR0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'g6FJMUy6ypmWtS3xDtpecrVexrqvCIGoFL4kLPTEj3o',
+      useTemplateId: 'pTTJuDziQiJwdJnNtcMJJSHUdDowlibfQsNfghICwXI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '11-13',
+      horoscopeDate: '08-28',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '严冬', year: '2002', date: '11-13',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '新叶', year: '2004', date: '08-28',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '04-22',
@@ -44,12 +44,12 @@
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-04-22' },
-        // // 结婚纪念日
-        // { keyword: 'marry_day', date: '2022-09-09' },
+        //  结婚纪念日
+         { keyword: 'The first day we met', date: '2023-04-13' },
       ],
     },
   ],
-
+  
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '',
