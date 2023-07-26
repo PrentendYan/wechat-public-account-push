7e090e89e5260d5474ff0974c59272d98028138f
Dong Yan <jimmy20021113@163.com>
2023-07-26 13:18:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oib9Z63xYxXx7rIi4mbE1Uw0ubR0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pTTJuDziQiJwdJnNtcMJJSHUdDowlibfQsNfghICwXI',
+      useTemplateId: 'g6FJMUy6ypmWtS3xDtpecrVexrqvCIGoFL4kLPTEj3o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-13',
       festivals: [
