c51f29828f8a0b13791b2660ef2fad6f8c515083
Dong Yan <jimmy20021113@163.com>
2023-08-11 10:45:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,7 +15,7 @@
   APP_SECRET: 'c9951636322280eaca625313636cd323',
 
   PROVINCE: '浙江',
-  CITY: '瑞安市',
+  CITY: '温州',
 
   USERS: [
     {
@@ -24,7 +24,7 @@
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oib9Z63xYxXx7rIi4mbE1Uw0ubR0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'uO7gy2aFX4YTOWPV9YzAHxj3IoDIfzPN6nWMmsvEX78',
+      useTemplateId: '	TxE0G8adME0YnSL9TAs9xDzA2KgCJaNvN96bYqHZMBM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-28',
       festivals: [
