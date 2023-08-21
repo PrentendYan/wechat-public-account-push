2f16b59c5353fd552aafb22ca8446bbb99d22c47
Dong Yan <jimmy20021113@163.com>
2023-08-21 16:42:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oib9Z63xYxXx7rIi4mbE1Uw0ubR0',
+      id: 'oib9Z65LBymqBbMsKksElI645mGE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ZJHLdWMRRy9-1_rG4VvhuzxMDruWVkVVNcodstMAg3E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
